14647f17552a574f4c3b21bd16fb7f4cd9b31da2
startoasia <sta_rt@startoasia.com>
2024-03-04 16:04:33 +0800

    loading改元件疑問

---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <VueLoading :active="isLoading" :z-index="1060">
+    <div class="loadingio-spinner-double-ring-0ut21surusy">
+      <div class="ldio-2uwwzn0tw8t">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  </VueLoading>
+</template>
+
+<script>
+export default {
+  props: ['loadingStatus'],
+  data () {
+    return {
+      isLoading: ''
+    }
+  },
+  watch: {
+    loadingStatus () {
+      console.log(this.loadingStatus)
+      this.isLoading = this.loadingStatus
+    }
+  }
+}
+</script>
---
+++ src/views/frontDesk/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <h1>Hello, This is Home Page.</h1>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
